b9bb8b3359a7df3f6720b758022a6b7600828e96
JoaoCruz20 <60579767+JoaoCruz20@users.noreply.github.com>
2025-11-21 16:44:10 +0000

    updated to version 19 and updated dependencies

--- src/index.js
+++ src/index.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+//import reportWebVitals from './reportWebVitals';
 import './index.css';
 import store from './store/store';
 import { Provider } from 'react-redux';
+import { createRoot } from 'react-dom/client';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = createRoot(document.getElementById('root'));
+root.render(
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+//reportWebVitals();
