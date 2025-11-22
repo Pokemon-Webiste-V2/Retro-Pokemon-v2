b6117dfdb5884669724581291857e68e06f506af
JoaoCruz20 <60579767+JoaoCruz20@users.noreply.github.com>
2025-11-22 18:03:39 +0000

    (F1): dependencies and files updated

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
 //import reportWebVitals from './reportWebVitals';
 import './index.css';
-import store from './store/store';
-import { Provider } from 'react-redux';
 import { createRoot } from 'react-dom/client';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
-    <Provider store={store}>
       <App />
-    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
